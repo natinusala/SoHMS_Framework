4a8059b96246e3dbd4fa58e3b9026dae2885e5ef
tebmed <lamin.teb94@gmail.com>
2018-04-24 16:28:03 +0200

    Adding of Resource Holon

--- SoHMS_Manufacturing_Software/src/ResourceManagement/ResourceHolon.java
+++ SoHMS_Manufacturing_Software/src/ResourceManagement/ResourceHolon.java
@@ -3,41 +3,80 @@ package ResourceManagement;
 import java.io.File;
 import java.util.ArrayList;
 
+import MService.MService;
 import MService.MServiceImplentation;
 
 public class ResourceHolon extends Resource{
 	
 	//attribute
 	protected static int rhCount= 0;
-	private ArrayList<MServiceImplentation> providedServices;
+	protected static int ListSize= 100;
+	
+	private ArrayList<MServiceImplentation> offeredServices;
 	
 
 	//Constructors
-	public ResourceHolon(){}
-	
-	//Getters
-	public ArrayList<MServiceImplentation> getProvidedServices() {
-		return providedServices;
-	}
-
-    //Setters
-	public void setProvidedServices(ArrayList<MServiceImplentation> providedServices) {
-		this.providedServices = providedServices;
-	}	
-	
-	//methods
-	public void initializeRessources(File F) {
-		/*
-		  le traitement ici est sp�cificifique � l'ontology products.
-		  le d�roulement de l'initialisation d�pend de la structure de d�claration des produit.
-		*/
+ 	public ResourceHolon(){
+		super();
+		this.resourceId= (rhCount % ListSize) +1;
+		rhCount=this.resourceId;
 	}
 	
-    public void addResource(String name,String technology, String category, String textDescription) {
+	public ResourceHolon(String name,String technology, String category, String textDescription) {
+		this();
 		this.name= name;
 		this.technology=technology;
 		this.category=category;
-		this.textDescription=textDescription;  
+		this.textDescription=textDescription;
+	}	
+	
+	//Getters
+	public ArrayList<MServiceImplentation> getOfferedServices() {
+		return offeredServices;
 	}
 
+    //Setters
+	public void setOfferedServices(ArrayList<MServiceImplentation> providedServices) {
+		this.offeredServices = providedServices;
+	}	
+	
+	//methods
+	public MService getServiceByName(String service) {
+		for (MServiceImplentation sr : offeredServices) {
+			if(sr.getmService().getName().equalsIgnoreCase(service)){
+				return sr.getmService();
+			}
+		}
+		return null;
+	}
+
+	public MServiceImplentation getServByType(MService service){
+		for (MServiceImplentation sr : offeredServices) {
+			if(sr.getmService().equals(service)){
+				return sr;
+			}
+		}
+		return null;
+	}
+  
+	public void addOfferedService(MServiceImplentation service) {
+		
+		if(offeredServices.size()==0){ 
+			this.offeredServices= new ArrayList<MServiceImplentation>(); 
+			this.offeredServices.add(service);
+		}
+		else{ 
+			
+		// Service already declared?	
+			for (int i = 0; i < this.offeredServices.size(); i++) {
+				if(offeredServices.get(i).getmService().equals(service.getmService())){
+					//It exists, then substitute
+						offeredServices.set(i, service);
+						break;
+					}
+			}
+			// It does not exists
+			offeredServices.add(service); // Then simply add the new Service Imp	
+		}
+	}	
 }
